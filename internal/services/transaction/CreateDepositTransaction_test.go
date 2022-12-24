aefe7dd1d779a89423e1704dcaecff1ca25866a9
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-12-24 12:54:14 -0300

    test: Add new test case in deposit transaction

--- internal/services/transaction/CreateDepositTransaction_test.go
+++ internal/services/transaction/CreateDepositTransaction_test.go
@@ -8,6 +8,37 @@ import (
 )
 
 func TestTransaction_NewDepositTransactionService(t *testing.T) {
+	type testCase struct {
+		name            string
+		customerName    string
+		cpf             string
+		initialBalance  decimal.Decimal
+		depositValue    decimal.Decimal
+		expectedBalance decimal.Decimal
+		expectedErr     error
+	}
+
+	testCases := []testCase{
+		{
+			name:            "Deposit with 0 of balance",
+			customerName:    "Vick",
+			cpf:             "000.000.000-00",
+			initialBalance:  decimal.NewFromInt(0),
+			depositValue:    decimal.NewFromInt(50),
+			expectedBalance: decimal.NewFromInt(50),
+			expectedErr:     nil,
+		},
+		{
+			name:            "Deposit with 50 of balance",
+			customerName:    "Vick",
+			cpf:             "000.000.000-00",
+			initialBalance:  decimal.NewFromInt(50),
+			depositValue:    decimal.NewFromInt(50),
+			expectedBalance: decimal.NewFromInt(100),
+			expectedErr:     nil,
+		},
+	}
+
 	ts, err := NewTransactionService(
 		WithMemoryCustomerRepository(),
 	)
@@ -15,27 +46,34 @@ func TestTransaction_NewDepositTransactionService(t *testing.T) {
 		t.Error(err)
 	}
 
-	// Add customer
-	customer, err := customer.NewCustomer("Vick", "000.000.000-00")
-	if err != nil {
-		t.Error(err)
-	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			customer, err := customer.NewCustomer(tc.customerName, tc.cpf)
+			if err != nil {
+				t.Error(err)
+			}
+			customer.SetBalance(tc.initialBalance)
+			err = ts.customers.Add(customer)
+			if err != nil {
+				t.Error(err)
+			}
+			err = ts.CreateDepositTransaction(customer.GetID(), tc.depositValue)
+			if err != nil {
+				t.Error(err)
+			}
 
-	err = ts.customers.Add(customer)
-	if err != nil {
-		t.Error(err)
-	}
+			customer, err = ts.customers.Get(customer.GetID())
+			if err != nil {
+				t.Error(err)
+			}
 
-	err = ts.CreateDepositTransaction(customer.GetID(), decimal.NewFromInt(50))
-	if err != nil {
-		t.Error(err)
-	}
+			if err != tc.expectedErr {
+				t.Errorf("Expected error %v, got %v", tc.expectedErr, err)
+			}
 
-	customer, err = ts.customers.Get(customer.GetID())
-	if err != nil {
-		t.Error(err)
-	}
-	if !customer.GetBalance().Equal(decimal.NewFromInt(50)) {
-		t.Errorf("Expected balance %v, got %v", decimal.NewFromInt(50), customer.GetBalance())
+			if !customer.GetBalance().Equal(tc.expectedBalance) {
+				t.Errorf("Expected balance %v, got %v", tc.expectedBalance, customer.GetBalance())
+			}
+		})
 	}
 }
