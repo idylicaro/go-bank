f2d544ea82e73f0bf5392a06d5d75d7cef7b5155
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2022-12-24 13:48:18 -0300

    refactor: Clear main

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/idylicaro/go-bank/internal/domain/customer"
-	"github.com/idylicaro/go-bank/internal/services/transaction"
 	utils "github.com/idylicaro/go-bank/internal/utils"
-	"github.com/shopspring/decimal"
 )
 
 func Hello() string {
@@ -14,20 +11,5 @@ func Hello() string {
 }
 
 func main() {
-	ts, err := transaction.NewTransactionService(
-		transaction.WithMemoryCustomerRepository(),
-	)
-	if err != nil {
-		panic(err)
-	}
-	customer, err := customer.NewCustomer("Idyl Icaro", "000.000.000-00")
-	if err != nil {
-		panic(err)
-	}
-	err = ts.CreateDepositTransaction(customer.GetID(), decimal.NewFromInt(1))
-	if err != nil {
-		panic(err)
-	}
-	fmt.Print(customer.GetBalance())
-	fmt.Print("HelloWorld!")
+	fmt.Print("Hello")
 }
